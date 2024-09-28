e506679409c7ef0082e14ea93c3f61a28fe23d11
seadonggyun4 <seadonggyun@gmail.com>
2024-09-28 16:28:19 +0900

    [style]:다운로드 버튼 스타일 변경

--- src/features/webviewDonland/components/DonloadBtn.tsx
+++ src/features/webviewDonland/components/DonloadBtn.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useWebViewDownload } from "../hook/useWebViewDownload";
 import backgroundImage from  '../../../asset/img/promotionPage.jpeg'
@@ -7,22 +7,130 @@ interface DonloadBtnProps {
     uploadedImage: string | ArrayBuffer | null; // 적절한 타입 지정
 }
 
+interface DownloadBtnStyleProps {
+    clicked: boolean;
+}
+
 function DonloadBtn({uploadedImage}: DonloadBtnProps) {
     const { webViewDownload } = useWebViewDownload(uploadedImage, backgroundImage);
+    const [clicked, setClicked] = useState(false);
+
+    const handleBtnClick = () => {
+        setClicked(prev => !prev);
+        webViewDownload()
+    }
 
     return (
-        <DownloadBtnStyle onClick={webViewDownload}>페이지 다운로드</DownloadBtnStyle>
+        <DownloadBtnStyle
+            onClick={handleBtnClick}
+            clicked={clicked}
+            type="button"
+            aria-label="프로모션 페이지 다운로드 버튼"
+        >
+            <div className="wrapper" aria-hidden>
+                <div className="front">페이지 다운로드</div>
+                <div className="top"></div>
+                <div className="right"></div>
+                <div className="bottom"></div>
+                <div className="left"></div>
+                <div className="back">페이지 다운로드</div>
+            </div>
+        </DownloadBtnStyle>
     )
 }
 
-const DownloadBtnStyle = styled.button`
-    padding: 0.3rem;
-    width: 150px;
-    height: 100%;
-    border-radius: 5px;
-    background: var(--c-accent-primary);
-    color: #ffffff;
-    box-shadow: 0 0 10px var(--c-accent-primary);
+const DownloadBtnStyle = styled.button<DownloadBtnStyleProps>`
+    position: relative;
+    perspective: calc(1rem * 10);
+    font-family: inherit;
+    font-size: 1rem;
+    font-weight: bold;
+    color: white;
+    background-color: transparent;
+    cursor: pointer;
+    -webkit-tap-highlight-color: transparent;
+    
+    &:focus-visible {
+        outline: 3px dashed var(--c-accent-primary);
+        outline-offset: calc(3px * 2);
+    }
+    
+    & .wrapper {
+        position: relative;
+        display: grid;
+        transform: ${({clicked}) => (clicked 
+                ? 'translateZ(calc(1rem * -1)) scale(1.001) rotateX(0) rotateY(0) rotateZ(0)' 
+                : 'translateZ(0) scale(1.001) rotateX(1.5turn) rotateY(0) rotateZ(0)')};
+        transform-style: preserve-3d;
+        pointer-events: none;
+        transition: transform 800ms cubic-bezier(0.3, 1.4, 0.65, 1);
+    }
+    
+    & .wrapper > * {
+        grid-area: 1 / 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transition: background-color 800ms cubic-bezier(0.3, 1.4, 0.65, 1);
+    }
+    
+    & .top,
+    & .bottom {
+        width: 100%;
+        height: 1rem;
+    }
+
+    & .left,
+    & .right {
+        width: 1rem;
+        height: 100%;
+    }
+
+    & .front {
+        transform: translateZ(1rem);
+    }
+
+    & .back {
+        transform: scaleX(-1) rotate(0.5turn);
+    }
+
+    & .top {
+        transform-origin: top center;
+        transform: rotateX(90deg);
+    }
+
+    & .bottom {
+        align-self: end;
+        transform-origin: bottom center;
+        transform: rotateX(-90deg);
+    }
+
+    & .right {
+        justify-self: end;
+        transform-origin: center right;
+        transform: rotateY(90deg);
+    }
+
+    & .left {
+        justify-self: start;
+        transform-origin: center left;
+        transform: rotateY(-90deg);
+    }
+
+    // 색상 설정
+    & .front,
+    & .back {
+        padding: 1rem 2rem;
+        background-color: ${(props) => (props.clicked 
+                ? 'var(--c-accent-success)' 
+                : 'var(--c-accent-primary)')};
+    }
+
+    & .wrapper :not(.front):not(.back) {
+        background-color: ${(props) => (props.clicked 
+                ? 'var(--c-accent-success-secondary)' 
+                : 'var(--c-text-action)')};
+    }
 `;
 
 
